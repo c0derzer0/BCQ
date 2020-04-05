90a48ef0c856d3df1c64eec30d1a3a9e17646872
c0derzer0 <contactme.teja@gmail.com>
2020-04-05 18:25:55 -0400

    Convert action from ndarray to int in eval_policy()

--- BCQ_for_RSPMN.py
+++ BCQ_for_RSPMN.py
@@ -153,6 +153,7 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
         state, done = eval_env.reset(), False
         while not done:
             action = policy.select_action(np.array(state))
+            action = int(action)
             state, reward, done, _ = eval_env.step(action)
             avg_reward += reward
 
